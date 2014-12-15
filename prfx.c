eb2da1c1a669835a2e7005464d18803a50f02ff0
Kyle Frost <kylefrost@comcast.net>
2014-12-15 16:06:56 -0500

    finds all lines with transform and prints them...progress

--- prfx.c
+++ prfx.c
@@ -18,6 +18,12 @@
 #include <ctype.h>
 #include <string.h>
 
+#ifdef DEBUG
+#define INITIAL_ALLOC 2
+#else
+#define INITIAL_ALLOC 512
+#endif
+
 void usage(char *program) {
     printf("Usage: %s <filename>\n", program);
 }
@@ -25,6 +31,46 @@ void usage(char *program) {
 float version() {
     return 1.0;
 }
+
+char *read_line(FILE *fin) {
+    char *buffer;
+    char *tmp;
+    int read_chars = 0;
+    int bufsize = INITIAL_ALLOC;
+    char *line = malloc(bufsize);
+
+    if (!line) {
+        return NULL;
+    }
+
+    buffer = line;
+
+    while (fgets(buffer, bufsize - read_chars, fin)) {
+        read_chars = strlen(line);
+
+        if (line[read_chars - 1] == '\n') {
+            line[read_chars - 1] = '\0';
+            return line;
+        }
+
+        else {
+            bufsize = 2 * bufsize;
+            tmp = realloc(line, bufsize);
+
+            if (tmp) {
+                line = tmp;
+                buffer = line + read_chars;
+            }
+            else {
+                free(line);
+                return NULL;
+            }
+        }
+    }
+
+    return NULL;
+}
+
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
@@ -39,15 +85,31 @@ int main(int argc, char *argv[]) {
     }
 
     FILE *css;
+    char *line;    
+    
+    char *transform = "transform";
+    char *transition = "transition";
 
-    css = fopen(argv[1], "r+");
+    css = fopen(argv[1], "r");
+    
+    if (css) {
+        while ((line = read_line(css))) {
+            if (strstr(line, transform)) {
+                printf("%s\n", line);
+            }
 
+            free(line);
+        }
+    }
+
+    fclose(css);
+
+    /*
     if (css == 0) {
         printf("Could not open file named \"%s\". Make sure it exists and was typed correctly.\n", argv[1]);
         exit(1);
     }
-
-    
+    */    
 
     return 0;
 }
