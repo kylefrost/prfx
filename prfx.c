bdcf68147dead66f6ec75675b189ea341e2b7e4b
Kyle Frost <kylefrost@comcast.net>
2014-12-15 14:51:11 -0500

    Add version arg and check if file exists #prfx

--- prfx.c
+++ prfx.c
@@ -18,12 +18,37 @@
 #include <ctype.h>
 #include <string.h>
 
+void usage(char *program) {
+    printf("Usage: %s <filename>\n", program);
+}
+
+float version() {
+    return 1.0;
+}
+
 int main(int argc, char *argv[]) {
-    if (argc != 1) {
-        printf
-
-
+    if (argc != 2) {
+        usage(argv[0]);
+        exit(1);
+    }
     
+    if (strcmp(argv[1], "-v") == 0) {
+        float vers = version();
+        printf("PRFX version %.01f\n", vers);
+        exit(1);
+    }
 
+    FILE *css;
+
+    css = fopen(argv[1], "r+");
+
+    if (css == 0) {
+        printf("Could not open file: %s.\n", argv[1]);
+        exit(1);
+    }
+    else {
+        fclose(css);
+    }
+            
     return 0;
 }
