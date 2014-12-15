5f8383e0f82e637b9e71f7fa030dafd6133122d0
Kyle Frost <kylefrost@comcast.net>
2014-12-15 14:22:14 -0500

    Add build script cause I'm lazy #prfx

---
+++ build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+gcc prfx.c -o prfx
--- prfx.c
+++ prfx.c
@@ -14,7 +14,16 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
+    if (argc != 1) {
+        printf
 
+
+    
+
+    return 0;
 }
