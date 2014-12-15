9d226f174a6551d85aa3ebc7bdfe8f5309fc0558
Kyle Frost <kylefrost@comcast.net>
2014-12-15 14:10:59 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# PRFX
+==================
+Prounounced "Pr(e)-f(i)x".
+
+Command line tool to automatically add prefixes for common CSS modifiers transform and transition.
+
+Still in development.
---
+++ prfx.c
@@ -0,0 +1,20 @@
+/*
+ *  prfx.c
+ *  
+ *  By:          Kyle Frost
+ *  Created:     10/15/2014
+ *
+ *  Description: Command line too that finds all
+ *               'transform' and 'transition' modifiers 
+ *               in CSS file and add browser specific 
+ *               prefixes (i.e. -webkit-, -moz-, -ms-).
+ *
+ *  Usage:       prfx style.css
+ *
+ */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+
+}
